14418aac828b725ca30dc2ffc8ca13fbee518d24
Aaron Stannard <aaron@petabridge.com>
2018-03-26 00:49:45 -0500

    added a spec to help guarantee good shutdown behavior

---
+++ src/Petabridge.Tracing.Zipkin.Tests/Reporting/HttpZipkinSpanReporterSpecs.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HttpZipkinSpanReporterSpecs.cs" company="Petabridge, LLC">
+//      Copyright (C) 2018 - 2018 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.TestKit.Xunit2;
+using FluentAssertions;
+using Petabridge.Tracing.Zipkin.Reporting.Http;
+using Xunit;
+
+namespace Petabridge.Tracing.Zipkin.Tests.Reporting
+{
+    public class HttpZipkinSpanReporterSpecs : TestKit
+    {
+        [Fact(DisplayName = "ZipkinHttpSpanReporter should not shut down an ActorSystem it does not own upon Dispose")]
+        public void ShouldNotShutDownNonOwnedActorSystem()
+        {
+            var zipkinSpanReporter =
+                ZipkinHttpSpanReporter.Create(new ZipkinHttpReportingOptions("localhost:9311"), Sys);
+            zipkinSpanReporter.Dispose();
+            Sys.WhenTerminated.IsCompleted.Should().BeFalse();
+        }
+    }
+}
