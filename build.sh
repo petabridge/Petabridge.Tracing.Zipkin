f1c436fe157e3eca3ebba4c90ceb268ddb274ac8
Aaron Stannard <aaron@petabridge.com>
2018-07-13 16:19:36 -0500

    fixed linux .NET Core install script (#45)
    * fixed linux .NET Core install script
    
    * made build.sh executable
    
    * fixed location of acqusition script

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ NUGET_EXE=$TOOLS_DIR/nuget.exe
 NUGET_URL=https://dist.nuget.org/win-x86-commandline/v4.0.0/nuget.exe
 FAKE_VERSION=4.61.2
 FAKE_EXE=$TOOLS_DIR/FAKE/tools/FAKE.exe
+DOTNET_EXE=$SCRIPT_DIR/.dotnet/dotnet
 DOTNET_VERSION=2.0.0
-DOTNET_INSTALLER_URL=https://raw.githubusercontent.com/dotnet/cli/2.0.0/scripts/obtain/dotnet-install.sh
+DOTNET_INSTALLER_URL=https://raw.githubusercontent.com/dotnet/cli/v2.0.0/scripts/obtain/dotnet-install.sh
 # Define default arguments.
 TARGET="Default"
 CONFIGURATION="Release"
@@ -41,12 +42,14 @@ fi
 # INSTALL .NET CORE CLI
 ###########################################################################
 
-echo "Installing .NET CLI..."
-if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-  mkdir "$SCRIPT_DIR/.dotnet"
+if [ ! -f "$DOTNET_EXE" ]; then
+    echo "Installing .NET CLI..."
+    if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
+      mkdir "$SCRIPT_DIR/.dotnet"
+    fi
+    curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" $DOTNET_INSTALLER_URL
+    bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION --install-dir .dotnet --no-path
 fi
-curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" $DOTNET_INSTALLER_URL
-bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION --install-dir .dotnet --no-path
 export PATH="$SCRIPT_DIR/.dotnet":$PATH
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
