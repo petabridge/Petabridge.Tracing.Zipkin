322f52e04634ee30a82955ebef8c2ced56c48011
Aaron Stannard <aaron@petabridge.com>
2018-08-24 19:20:34 -0500

    adding kafka reporting actor

---
+++ src/Petabridge.Tracing.Zipkin/Reporting/Kafka/KafkaReportingActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Phobos.Actor.Common;
+
+namespace Petabridge.Tracing.Zipkin.Reporting.Kafka
+{
+    /// <summary>
+    /// INTERNAL API
+    ///  Actor responsible for managing the batching of outbound <see cref="T:Petabridge.Tracing.Zipkin.Span" /> instances.
+    /// </summary>
+    internal sealed class KafkaReportingActor : ReceiveActor, INeverMonitor, INeverTrace
+    {
+    }
+}
