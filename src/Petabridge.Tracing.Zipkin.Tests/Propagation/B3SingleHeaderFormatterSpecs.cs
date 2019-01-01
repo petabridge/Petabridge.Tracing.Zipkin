ac9b34835f042efd48fb5570b8efa0683a8c3516
Aaron Stannard <aaron@petabridge.com>
2019-01-01 17:52:35 -0600

    added parsing test cases

--- src/Petabridge.Tracing.Zipkin.Tests/Propagation/B3SingleHeaderFormatterSpecs.cs
+++ src/Petabridge.Tracing.Zipkin.Tests/Propagation/B3SingleHeaderFormatterSpecs.cs
@@ -58,6 +58,16 @@ namespace Petabridge.Tracing.Zipkin.Tests.Propagation
             b3Header.Should().Be(Encoding.UTF8.GetString(WriteB3SingleFormatAsBytes(context)));
         }
 
+        [Fact(DisplayName = "Should write 64bit B3 single format header with a parent id only")]
+        public void ShouldWriteB3SingleHeaderWithParentIdOnly()
+        {
+            var context = new SpanContext(new TraceId(1), 3, parentId: Tracer.IdProvider.NextSpanId());
+
+            var b3Header = WriteB3SingleFormat(context);
+            b3Header.Should().Be(context.TraceId + "-" + context.SpanId + "-" + context.ParentId);
+            b3Header.Should().Be(Encoding.UTF8.GetString(WriteB3SingleFormatAsBytes(context)));
+        }
+
         [Fact(DisplayName = "Should write 128bit B3 single format header without sampling")]
         public void ShouldWriteB3SingleHeaderWithoutSampling128Bit()
         {
@@ -86,6 +96,147 @@ namespace Petabridge.Tracing.Zipkin.Tests.Propagation
             var b3Header = WriteB3SingleFormat(context);
             b3Header.Should().Be(context.TraceId + "-" + context.SpanId + "-1-" + context.ParentId);
             b3Header.Should().Be(Encoding.UTF8.GetString(WriteB3SingleFormatAsBytes(context)));
+        }
+
+        [Fact(DisplayName = "Should write 128bit B3 single format header with parent id only")]
+        public void ShouldWriteB3SingleHeaderWithParentIdOnly128Bit()
+        {
+            var context = new SpanContext(new TraceId(1, 1), 3, parentId: Tracer.IdProvider.NextSpanId());
+
+            var b3Header = WriteB3SingleFormat(context);
+            b3Header.Should().Be(context.TraceId + "-" + context.SpanId + "-" + context.ParentId);
+            b3Header.Should().Be(Encoding.UTF8.GetString(WriteB3SingleFormatAsBytes(context)));
+        }
+
+
+        [Fact(DisplayName = "Should parse 128bit B3 single format header with sampling and parent id (max length)")]
+        public void ShouldParseB3SingleHeaderWithSamplingAndParentId128Bit()
+        {
+            var context = new SpanContext(new TraceId(1, 1), 3, parentId: Tracer.IdProvider.NextSpanId(), sampled: true);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 128bit B3 single format header with sampling")]
+        public void ShouldParseB3SingleHeaderWithSampling128Bit()
+        {
+            var context = new SpanContext(new TraceId(1, 1), 3, sampled: true);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 128bit B3 single format header with debug")]
+        public void ShouldParseB3SingleHeaderWithDebug128Bit()
+        {
+            var context = new SpanContext(new TraceId(1, 1), 3, debug: true);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 128bit B3 single format header")]
+        public void ShouldParseB3SingleHeader128Bit()
+        {
+            var context = new SpanContext(new TraceId(1, 1), 3);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 128bit B3 single format header with parentId only")]
+        public void ShouldParseB3SingleHeaderParentIdOnly128Bit()
+        {
+            var context = new SpanContext(new TraceId(1, 1), 3, parentId: Tracer.IdProvider.NextSpanId());
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 64bit B3 single format header with sampling and parent id")]
+        public void ShouldParseB3SingleHeaderWithSamplingAndParentId()
+        {
+            var context = new SpanContext(new TraceId(1), 3, parentId: Tracer.IdProvider.NextSpanId(), sampled: true);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 64bit B3 single format header with sampling")]
+        public void ShouldParseB3SingleHeaderWithSampling()
+        {
+            var context = new SpanContext(new TraceId(1), 3, sampled: true);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 64bit B3 single format header with debug")]
+        public void ShouldParseB3SingleHeaderWithDebug()
+        {
+            var context = new SpanContext(new TraceId(1), 3, debug: true);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 64bit B3 single format header")]
+        public void ShouldParseB3SingleHeader()
+        {
+            var context = new SpanContext(new TraceId(1), 3);
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
+        }
+
+        [Fact(DisplayName = "Should parse 64bit B3 single format header with parent id")]
+        public void ShouldParseB3SingleHeaderWithParentId()
+        {
+            var context = new SpanContext(new TraceId(1), 3, parentId: Tracer.IdProvider.NextSpanId());
+
+            var b3Header = WriteB3SingleFormat(context);
+            var b3HeaderParsed = ParseB3SingleFormat(b3Header);
+
+            b3HeaderParsed.Should().Be(context);
+            b3HeaderParsed.Sampled.Should().Be(context.Sampled);
+            b3HeaderParsed.Debug.Should().Be(context.Debug);
         }
     }
 }
