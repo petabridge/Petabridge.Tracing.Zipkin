fa133e1255edbe0b68c45f2dc8168aa23bd29dad
Aaron Stannard <aaron@petabridge.com>
2019-08-06 14:36:02 -0500

    Disable strong naming (#100)
    * close #94 - disable strong naming
    
    * changed InternalsVisibleTo

--- src/Petabridge.Tracing.Zipkin/Properties/Friends.cs
+++ src/Petabridge.Tracing.Zipkin/Properties/Friends.cs
@@ -6,8 +6,11 @@
 
 using System.Runtime.CompilerServices;
 
+/*
 [assembly: InternalsVisibleTo("Petabridge.Tracing.Zipkin.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000179e21e365117"+
 "e1758def0b71a5d19119ab26d983000a8d76cab1adce4e3f6482b006b2a4f6783a2d95fe31c972" +
 "5f1fe455bb88b0b98f37489a726cbf48918c7dea2d0fe96aeaaab5ea91767918325c0e576863f3" +
 "e72c10f4d61daee4886bc9d7fa2dd6039cea0bb935bb8fc103a3d387128e25ac5b29ed637b7cfa" +
 "acceaeb2")]
+*/
+[assembly: InternalsVisibleTo("Petabridge.Tracing.Zipkin.Tests")]
